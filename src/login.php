f2a8a0a6d49ddb4dc12078616f4d350f28c43205
yoonkyoungchoi <63581073+yoonkyoungchoi@users.noreply.github.com>
2021-06-23 03:12:39 +0900

    admin 페이지 비밀번호 설정 성공

--- src/login.php
+++ src/login.php
@@ -1,21 +1,20 @@
 <?php
-    include('db_conn.php');
     $password = $_POST[ 'password' ];
-    
-    $sql = "SELECT password FROM qna WHERE password is not null";
-    $result = mysqli_query( $conn, $sql );
+    $jb_conn = mysqli_connect('localhost','map','2LjB9XqPE7jj2mpa','map');
+    $jb_sql = "SELECT password FROM qna WHERE password is not null";
+    $jb_result = mysqli_query($jb_conn, $jb_sql);
 
-    while ($row = mysqli_fetch_array($result)) {
-        $encrypted_password = $row['password'];
+    while ($jb_row = mysqli_fetch_array( $jb_result) ) {
+      $encrypted_password = $jb_row['password'];
     }
-    if ( is_null( $encrypted_password ) ) {
-        $wu = 1;
+    if (is_null($encrypted_password)) {
+      $wu = 1;
     } else {
-        if ( password_verify( $password, $encrypted_password ) ) {
-        header( 'Location:https://map.emirim.kr/src/db_test_admin.php' );
-        } else {
+      if($password == $encrypted_password) {
+        header('Location:https://map.emirim.kr/src/db_test_admin.php');
+      } else {
         $wp = 1;
-        }
+      }
     }
 ?>
 <!doctype html>
@@ -29,17 +28,15 @@
     </style>
   </head>
   <body>
-    <center>
     <h1>로그인</h1>
     <form action="login.php" method="POST">
       <p><input type="password" name="password" placeholder="비밀번호" required></p>
       <p><input type="submit" value="로그인"></p>
       <?php
         if ( $wp == 1 ) {
-            echo "<script>alert('비밀번호가 틀렸습니다.');</script>";
+            echo "<script>alert('비밀번호가 틀렸습ㄴ디다.');</script>";
         }
       ?>
     </form>
-    </center>
   </body>
 </html>
