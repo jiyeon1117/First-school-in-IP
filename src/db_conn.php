9e0dda754ad9849ed1936ad79cac13907dc19076
yoonkyoungchoi <s2019w40@e-mirim.hs.kr>
2021-06-24 10:30:15 +0900

    글 목록 쿼리 수정

--- src/db_conn.php
+++ src/db_conn.php
@@ -8,4 +8,6 @@ if($conn){
 mysqli_query($conn, "set session character_set_connection=utf8;");
 mysqli_query($conn, "set session character_set_results=utf8;");
 mysqli_query($conn, "set session character_set_client=utf8;");
+mysqli_query($conn, "set @cnt = 0;");
+mysqli_query($conn, "update qna set qna.num = @cnt:=@cnt+1;");
 ?>
